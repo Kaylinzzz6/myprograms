fd33c3a3bee7001ede9b4810806bf519c9575929
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-09-15 10:51:30 -0400

    created a python file to be creepy.

---
+++ p1.py
@@ -0,0 +1,9 @@
+# my first program
+# Created on 09/15/2025
+# Pyrpose: to learn python
+# Editor: Kaylin
+
+name = input("Hihi, what's your name?")
+age = input("Alr then how old are you?")
+print(f"Hey, {name} it's so cool that you're {age}")
+
