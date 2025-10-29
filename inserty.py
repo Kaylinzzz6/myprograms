1730348f11fb79a836fb13db44ad7a95e02a9639
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-10-28 21:10:05 -0400

    10/28/2025

--- inserty.py
+++ inserty.py
@@ -1,11 +1,14 @@
 def inserty(listy):
     i = 1
     while i < len(listy):
-        j = i
+        j = i 
         while j > 0:
             if listy[j-1] > listy[j]:
-                listy[j-1], listy[j], listy[j-1]
-                else:
-                    break
-                j -= 1 #j = j - 1
-            i += 1
+                listy[j-1], listy[j] = listy[j], listy[j-1]
+            else:
+                break
+            j -= 1
+        i += 1
+    return listy
+
+print(inserty([6,67,8,3,5,234234,6,74,341,43,7,75,2,42,6,35,1,4,3,]))
